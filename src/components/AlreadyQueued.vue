db00fbf45dd043ff578ba72f2853f6a9e2fee34e
Christoph Stahl <christoph.stahl@tu-dortmund.de>
2023-04-06 00:18:47 +0200

    missing vue file

---
+++ src/components/AlreadyQueued.vue
@@ -0,0 +1,13 @@
+<script setup>
+const emits = defineEmits(["append", "wait", "cancel"])
+</script>
+<template>
+<div class="reveal" id="alreadyqueued" data-reveal >
+<h1>You are already in queue</h1>
+<p>While you can append the song anyway, you can also queue it to the "waiting room".</p>
+<p>It will be appended to the queue as soon, as your next song leaves the queue.</p>
+<button class="button" @click="$emit('append')">Append</button>
+<button class="button" @click="$emit('wait')">Waiting Room</button>
+<button class="button" @click="$emit('cancel')">Abort</button>
+</div>
+</template>
